4af78d88ba88e83b7931ffff852464d1a06cd3bd
Robert D. French <robert@robertdfrench.me>
2021-05-30 13:35:07 -0400

    Using illumos threads to support two apps

---
+++ src/examples/caasio.c
@@ -0,0 +1,56 @@
+#include <door.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <strings.h>
+#include <err.h>
+
+char handle_request(int client_fd) {
+	// Get the client's name from the request
+	char name[32];
+
+	ssize_t name_len = read(client_fd, name, 31);
+	if (name_len == -1) return 1; // Error 1: Client read failure.
+
+	name[name_len] = 0; // Append a null byte so the string is proper.
+
+	// Greet the client
+	char greeting[64];
+
+	int greeting_len = snprintf(greeting, 64, "Hello %s, I'm Caasio!\n", name);
+	if (greeting_len < 0) return 2; // Error 2: Formatting failure.
+
+	ssize_t bytes_written = write(client_fd, greeting, greeting_len);
+	if (bytes_written == -1) return 3; // Error 3: Client write failure.
+
+	return 0;
+}
+
+void answer_door(void* cookie, char* args, size_t nargs,
+		door_desc_t* descriptors, uint_t ndescriptors) {
+	// Error 4: No client file descriptor was provided
+	char error_4 = 4;
+	if (ndescriptors == 0) door_return(&error_4, 1, NULL, 0);
+
+	int client_fd = descriptors[0].d_data.d_desc.d_descriptor;
+
+	char rc = handle_request(client_fd);
+	if (close(client_fd) == -1) rc = 5; // Error 5: Cannot close connection
+	door_return(&rc, 1, NULL, 0);
+}
+
+int main() {
+	char* path = "/var/run/caasio_door";
+
+	int door = door_create(&answer_door, NULL, 0);
+	if (door == -1) err(1, "Handle cannot be attached to door");
+
+	int fd = open(path, O_RDWR|O_CREAT|O_EXCL, 0400);
+	if (fd < 0) err(1, "Could not create a new file for the door");
+
+	int attachment = fattach(door, path);
+	if (attachment == -1) err(1, "Could not attach door to filesystem");
+
+	return door_return(NULL, 0, NULL, 0);
+}
--- src/relaydoors.c
+++ src/relaydoors.c
@@ -12,52 +12,94 @@
 #include <unistd.h>
 #include <string.h>
 #include <syslog.h>
+#include <thread.h>
 
-const unsigned short port = 8080;
-const char ip4_address[16] = "0.0.0.0";
-const char door_path[128] = "/var/run/hello_web_door";
+typedef struct _relay_plan {
+    char ip4_address[16];
+    unsigned short port;
+    char door_path[128];
+} RelayPlan;
+
+typedef struct _relay_link {
+    int application_descriptor;
+    int network_descriptor;
+} RelayLink;
+
+RelayLink establish_relay_link(RelayPlan rp) {
+    int application_descriptor = open(rp.door_path, O_RDONLY);
+    if (application_descriptor == -1) err(1, "Could not open application door");
+
+    int network_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (network_descriptor == -1) err(1, "Could not open client connection");
+
+    struct sockaddr_in in_sock;
+    in_sock.sin_family = AF_INET;
+    in_sock.sin_port = htons(rp.port);
+    int pton_rc = inet_pton(AF_INET, rp.ip4_address, &in_sock.sin_addr);
+    if (pton_rc != 1) err(1, "Could not convert ip string to network addr");
+
+    int bind_rc = bind(network_descriptor, (struct sockaddr*)&in_sock,
+            sizeof(struct sockaddr_in));
+    if (bind_rc == -1) err(1, "Could not bind socket to network address");
+
+    int listen_rc = listen(network_descriptor, 128);
+    if (listen_rc == -1) err(1, "Could not begin listening to network");
+
+    RelayLink rl = { application_descriptor, network_descriptor };
+    return rl;
+}
+
+void* relay_loop(void* link_ptr) {
+    RelayLink rl = *((RelayLink*)link_ptr);
+
+    while(1) {
+        struct sockaddr_in out_sock;
+        socklen_t out_socklen = sizeof(struct sockaddr);
+        int client_fd = accept(rl.network_descriptor,
+                (struct sockaddr*)&out_sock, &out_socklen);
+        if (client_fd == -1) err(1, "Could not accept client connection");
+
+        // Prepare door args with client_fd
+        door_desc_t w_descriptor;
+        w_descriptor.d_attributes = DOOR_DESCRIPTOR;
+        w_descriptor.d_data.d_desc.d_descriptor = client_fd;
+        door_arg_t args = {0};
+        args.desc_ptr = &w_descriptor;
+        args.desc_num = 1;
+
+        int result;
+        result = door_call(rl.application_descriptor, &args);
+        if (result == -1) err(1, "Could not invoke application via its door");
+
+        result = close(client_fd);
+        if (result == -1) err(1, "Could not terminate client");
+    }
+
+    return NULL;
+}
 
 int main() {
-	int application = open(door_path, O_RDONLY);
-	if (application == -1) err(1, "Could not open application door");
+    // Get this from argv or config file eventually
+    const uint8_t num_links = 2;
+    const RelayPlan plans[num_links] = {
+        { "0.0.0.0", 8080, "/var/run/hello_web_door" },
+        { "0.0.0.0", 1234, "/var/run/caasio_door" }
+    };
+    RelayLink links[num_links];
 
-	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1) err(1, "Could not open client connection");
-
-	struct sockaddr_in in_sock;
-	in_sock.sin_family = AF_INET;
-	in_sock.sin_port = htons(port);
-	int pton_rc = inet_pton(AF_INET, ip4_address, &in_sock.sin_addr);
-	if (pton_rc != 1) err(1, "Could not convert ip string to network addr");
-
-	int bind_rc = bind(socket_fd, (struct sockaddr*)&in_sock, sizeof(struct sockaddr_in));
-	if (bind_rc == -1) err(1, "Could not bind socket to network address");
-
-	int listen_rc = listen(socket_fd, 128);
-	if (listen_rc == -1) err(1, "Could not begin listening to network");
-
-	while(1) {
-		struct sockaddr_in out_sock;
-		socklen_t out_socklen = sizeof(struct sockaddr);
-		int client_fd = accept(socket_fd, (struct sockaddr*)&out_sock, &out_socklen);
-		if (client_fd == -1) err(1, "Could not accept client connection");
-
-		// Prepare door args with client_fd
-		door_desc_t w_descriptor;
-		w_descriptor.d_attributes = DOOR_DESCRIPTOR;
-		w_descriptor.d_data.d_desc.d_descriptor = client_fd;
-		door_arg_t args = {0};
-		args.desc_ptr = &w_descriptor;
-		args.desc_num = 1;
-
-		int result;
-		result = door_call(application, &args);
-		if (result == -1) err(1, "Could not invoke application via its door");
-
-		result = close(client_fd);
-		if (result == -1) err(1, "Could not terminate client");
-	}
+    // Establish each relay link
+    for (int i = 0; i < num_links; i++) {
+        links[i] = establish_relay_link(plans[i]);
+    }
 
 
-	return 0;
+    thread_t threads[num_links];
+    for (int i = 0; i < num_links; i++) {
+        thr_create(NULL, 0, relay_loop, &links[i], 0, &threads[i]);
+    }
+
+    while (thr_join(0, NULL, NULL) == 0)
+        continue;
+
+    return 0;
 }
