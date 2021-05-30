7a83db298b3c055003a8750c6d7c854892756290
Robert D. French <robert@robertdfrench.me>
2021-05-30 03:08:05 -0400

    Responding to network requests!

--- src/relaydoors.c
+++ src/relaydoors.c
@@ -1,6 +1,9 @@
 #include <door.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -18,23 +21,42 @@ int main() {
 	int application = open(door_path, O_RDONLY);
 	if (application == -1) err(1, "Could not open application door");
 
-	int client_fd = open("./junk_name", O_RDWR);
-	if (client_fd == -1) err(1, "Could not open client connection");
+	int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd == -1) err(1, "Could not open client connection");
 
-	// Prepare door args with client_fd
-	door_desc_t w_descriptor;
-	w_descriptor.d_attributes = DOOR_DESCRIPTOR;
-	w_descriptor.d_data.d_desc.d_descriptor = client_fd;
-	door_arg_t args = {0};
-	args.desc_ptr = &w_descriptor;
-	args.desc_num = 1;
+	struct sockaddr_in in_sock;
+	in_sock.sin_family = AF_INET;
+	in_sock.sin_port = htons(port);
+	int pton_rc = inet_pton(AF_INET, ip4_address, &in_sock.sin_addr);
+	if (pton_rc != 1) err(1, "Could not convert ip string to network addr");
 
-	int result;
-	result = door_call(application, &args);
-	if (result == -1) err(1, "Could not invoke application via its door");
+	int bind_rc = bind(socket_fd, (struct sockaddr*)&in_sock, sizeof(struct sockaddr_in));
+	if (bind_rc == -1) err(1, "Could not bind socket to network address");
 
-	result = close(client_fd);
-	if (result == -1) err(1, "Could not terminate client");
+	int listen_rc = listen(socket_fd, 128);
+	if (listen_rc == -1) err(1, "Could not begin listening to network");
+
+	while(1) {
+		struct sockaddr_in out_sock;
+		socklen_t out_socklen = sizeof(struct sockaddr);
+		int client_fd = accept(socket_fd, (struct sockaddr*)&out_sock, &out_socklen);
+		if (client_fd == -1) err(1, "Could not accept client connection");
+
+		// Prepare door args with client_fd
+		door_desc_t w_descriptor;
+		w_descriptor.d_attributes = DOOR_DESCRIPTOR;
+		w_descriptor.d_data.d_desc.d_descriptor = client_fd;
+		door_arg_t args = {0};
+		args.desc_ptr = &w_descriptor;
+		args.desc_num = 1;
+
+		int result;
+		result = door_call(application, &args);
+		if (result == -1) err(1, "Could not invoke application via its door");
+
+		result = close(client_fd);
+		if (result == -1) err(1, "Could not terminate client");
+	}
 
 
 	return 0;
