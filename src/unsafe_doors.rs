086f0271f03bb848529862988950fb077c350cc1
Robert D. French <robert@robertdfrench.me>
2021-05-31 21:58:05 -0400

    Explain the subset of Doors API that we use.

--- src/unsafe_doors.rs
+++ src/unsafe_doors.rs
@@ -1,40 +1,17 @@
+//! Unsafe Wrapper for the Doors API
+//!
+//! This module merely re-exports the subset of the [illumos doors
+//! api](https://github.com/robertdfrench/revolving-door) that we need
+//! for this project. It makes no attempt at safety or ergonomics. 
+
+
 #![allow(non_camel_case_types)]
 use libc;
 
 
-pub type door_attr_t = libc::c_uint;
-pub type door_id_t = libc::c_ulonglong;
-
-#[derive(Copy,Clone)]
-#[repr(C)]
-pub struct door_desc_t__d_data__d_desc {
-    pub d_descriptor: libc::c_int,
-    pub d_id: door_id_t
-}
-
-#[repr(C)]
-pub struct door_desc_t {
-    pub d_attributes: door_attr_t,
-    pub d_data: door_desc_t__d_data,
-}
-
-#[repr(C)]
-pub union door_desc_t__d_data {
-    pub d_desc: door_desc_t__d_data__d_desc,
-    d_resv: [libc::c_int; 5], /* Check out /usr/include/sys/door.h */
-}
-
-#[repr(C)]
-pub struct door_arg_t {
-    pub data_ptr: *const libc::c_char,
-    pub data_size: libc::size_t,
-    pub desc_ptr: *const door_desc_t,
-    pub desc_num: libc::c_uint,
-    pub rbuf: *const libc::c_char,
-    pub rsize: libc::size_t,
-}
-
 extern "C" {
+    /// Turns a function into a file descriptor.  See
+    /// [DOOR_CREATE(3C)](https://illumos.org/man/3C/door_create).
     pub fn door_create(
         server_procedure: extern "C" fn(
             cookie: *const libc::c_void,
@@ -47,22 +24,98 @@ extern "C" {
         attributes: door_attr_t,
     ) -> libc::c_int;
 
+
+    /// Invokes a function in another process (assuming `d` is a file
+    /// descriptor for a door which points to a function in another
+    /// process).  See
+    /// [DOOR_CALL(3C)](https://illumos.org/man/3C/door_call).
     pub fn door_call(
         d: libc::c_int,
         params: *const door_arg_t
     ) -> libc::c_int;
 
+
+    /// The inverse of `door_call`. Use this at the end of
+    /// `server_procedure` in lieu of the traditional `return` statement
+    /// to transfer control back to the process which originally issued 
+    /// `door_call`. See
+    /// [DOOR_RETURN(3C)](https://illumos.org/man/3C/door_return).
     pub fn door_return(
         data_ptr: *const libc::c_char,
         data_size: libc::size_t,
         desc_ptr: *const door_desc_t,
         num_desc: libc::c_uint,
-    ) -> !;
+    ) -> !; // Like EXIT(3C) or EXECVE(2), this function is terminal.
 
+
+    /// Makes a door descriptor visible on the filesystem. Just like
+    /// sockets must be created (as descriptors) and THEN attached to an
+    /// IP Address + Port Number by calling BIND(3SOCKET), doors are
+    /// created (as descriptors) and THEN attached to a path on the
+    /// filesystem by calling
+    /// [FATTACH(3C)](https://illumos.org/man/3c/fattach).
     pub fn fattach(
         fildes: libc::c_int,
         path: *const libc::c_char,
     ) -> libc::c_int;
+}
+
+
+/// For our purposes, this data structure and its constituent parts are
+/// mostly opaque *except* that it holds any file / socket / door
+/// descriptors which we would like to pass between processes. Rust does
+/// not support nested type declaration like C does, so we define each
+/// component separately. See
+/// [doors.h](https://github.com/illumos/illumos-gate/blob/9ecd05bdc59e4a1091c51ce68cce2028d5ba6fd1/usr/src/uts/common/sys/door.h#L122)
+/// for the original (nested) definition of this type and [Revolving
+/// Doors](https://github.com/robertdfrench/revolving-door/tree/master/A0_result_parameters)
+/// for a visual dissection.
+#[repr(C)]
+pub struct door_desc_t {
+    pub d_attributes: door_attr_t,
+    pub d_data: door_desc_t__d_data,
+}
+
+
+/// Door behavior options, as specified in the "Description" section of
+/// [DOOR_CREATE(3C)](https://illumos.org/man/3c/door_create).
+pub type door_attr_t = libc::c_uint;
+
+
+/// This is not a real doors data structure *per se*, but rather the
+/// `d_data` component of the `door_dest_t` type. It is defined in
+/// [doors.h](https://github.com/illumos/illumos-gate/blob/9ecd05bdc59e4a1091c51ce68cce2028d5ba6fd1/usr/src/uts/common/sys/door.h#L124).
+#[repr(C)]
+pub union door_desc_t__d_data {
+    pub d_desc: door_desc_t__d_data__d_desc,
+    d_resv: [libc::c_int; 5], /* Check out /usr/include/sys/door.h */
+}
+
+/// This is the `d_desc` component of the `d_data` union of the
+/// `door_desct_t` structure. See its definition in
+/// [doors.h](https://github.com/illumos/illumos-gate/blob/9ecd05bdc59e4a1091c51ce68cce2028d5ba6fd1/usr/src/uts/common/sys/door.h#L129).
+#[derive(Copy,Clone)]
+#[repr(C)]
+pub struct door_desc_t__d_data__d_desc {
+    pub d_descriptor: libc::c_int,
+    pub d_id: door_id_t
+}
+
+
+/// Some kind of door identifier. The doors API handles this for us, we
+/// don't really need to worry about it. Or at least, if I should be
+/// worried about it, I'm in a lot of trouble.
+pub type door_id_t = libc::c_ulonglong;
+
+
+#[repr(C)]
+pub struct door_arg_t {
+    pub data_ptr: *const libc::c_char,
+    pub data_size: libc::size_t,
+    pub desc_ptr: *const door_desc_t,
+    pub desc_num: libc::c_uint,
+    pub rbuf: *const libc::c_char,
+    pub rsize: libc::size_t,
 }
 
 #[cfg(test)]
