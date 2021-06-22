7cf3708a1957d719eaef0e95c7ebc3923949dee0
Robert D. French <robert@robertdfrench.me>
2021-06-21 23:11:31 -0400

    Use `polling` crate, per @papertigers
    Mr. Tigers suggested that https://crates.io/crates/polling might be
    worth a look. Using this to watch for new connections, then allowing
    rayon to handle individual requests in a blocking fashion -- looking at
    creating a shitload of threads each with a tiny stack size, since the
    real work will be done on the other side of the door.

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@
  */
 //! Portunus Daemon
 
+use polling::{Event, Poller};
 use portunusd::door;
 use portunusd::plan;
 use rayon;
 use std::io::{Read,Write};
 use std::net::{TcpListener,TcpStream};
-use std::thread;
-use std::time::Duration;
 
 
-struct RelayPath {
+struct Relay {
     pub port: TcpListener,
     pub door: door::Client,
 }
@@ -28,39 +27,44 @@ fn main() {
     ];
 
 
-    let mut paths = vec![];
+    let mut relays = vec![];
     for statement in &relay_statements {
         let plan: plan::RelayPlan = statement.parse().unwrap();
-        paths.push(RelayPath{
+        relays.push(Relay{
             door: door::Client::new(&plan.application_path.to_str().unwrap()).unwrap(),
             port: TcpListener::bind(&plan.network_address).unwrap(),
         });
     }
 
-
-    while let Some(path) = paths.pop() {
-        rayon::spawn(|| {
-            monitor(path);
-        });
+    let poller = Poller::new().unwrap();
+    for (key,relay) in relays.iter().enumerate() {
+        relay.port.set_nonblocking(true).unwrap();
+        poller.add(&relay.port, Event::readable(key)).unwrap();
     }
 
-    thread::sleep(Duration::new(u64::MAX, 1_000_000_000 - 1));
-}
+    let mut events = Vec::new();
+    loop {
+        events.clear();
+        poller.wait(&mut events, None).unwrap();
 
-fn monitor(path: RelayPath) {
-    for stream in path.port.incoming() {
-        let stream = stream.unwrap();
-        let client = path.door.borrow();
+        for event in &events {
+            // A new client has arrived
+            let relay = &relays[event.key]; // We know this exists b/c enumerate
+            let (stream, _) = relay.port.accept().unwrap();
 
-        rayon::spawn(|| {
-            handle_connection(stream, client);
-        });
+            let client = relay.door.borrow();
+            rayon::spawn(|| {
+                handle_connection(stream, client);
+            });
+
+            poller.modify(&relay.port, Event::readable(event.key)).unwrap();
+        }
     }
 }
-
 
 fn handle_connection(mut stream: TcpStream, client: door::ClientRef) {
     let mut request = [0; 1024];
+    stream.set_nonblocking(false).unwrap();
     stream.read(&mut request).unwrap();
 
     let response = client.call(&request).unwrap();
